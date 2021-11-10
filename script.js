b57ff4948ede740bda717b2575b286ecdf1a7d84
Sarath Radhakrishnan <sarathrk1@outlook.com>
2021-11-10 10:35:02 +0530

    Commited the final code.

---
+++ script.js
@@ -0,0 +1,29 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const newYears = "1 Jan 2022";
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentdDate = new Date();
+
+    const totalseconds = (newYearsDate - currentdDate)/1000;
+
+    const days = Math.floor(totalseconds/3600/24);
+    const hours = Math.floor(totalseconds/3600)%24;
+    const minutes = Math.floor(totalseconds/60)%60;
+    const seconds = Math.floor(totalseconds%60);
+    
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+//Initial call
+countdown();
+setInterval(countdown,1000);
